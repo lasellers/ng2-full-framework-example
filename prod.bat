fab7d53a2983baad6bae74d59acd774919e9e065
Lewis A. Sellers <lasellers@gmail.com>
2017-02-19 17:38:02 -0500

    build dist

--- prod.bat
+++ prod.bat
@@ -1,7 +1,9 @@
+# git checkout gh-pages
 ng build -prod --base-href="/ng2-full-framework-example/"
 git add dist -f
 git status
 git commit -am "build dist"
+git status
 git subtree push --prefix=dist origin gh-pages
 git checkout master -f
 git branch
